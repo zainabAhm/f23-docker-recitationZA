df805cb61dbaefabd905154acb45437a87a3bf86
tlee0818 <texeirareborn@gmail.com>
2023-03-16 01:26:53 -0400

    more starter code

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from typing import Union
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
@@ -11,6 +12,7 @@ def read_root():
 
 RECITATION_HOURS = {"a": "09:00~09:50", "b": "10:00~10:50",
                     "c": "11:00~11:50", "d": "12:00~12:50"}
+MICROSERVICE_LINK = "https://whos-my-ta.fly.dev/section_id/"
 
 
 @app.get("/section_info/{section_id}")
@@ -20,6 +22,11 @@ def get_section_info(section_id: str):
         raise HTTPException(status_code=404, detail="Missing section id")
 
     section_id = section_id.lower()
+
+    r = requests.get("https://whos-my-ta.fly.dev/section_id/" + section_id)
+
+    # You can check out what the response body looks like in terminal using the print statement
+    print(r.content)
 
     # TODO
 
